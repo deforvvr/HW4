a41eddb6613a7f7ea7225e15088fc79fa212fbee
Egor <eeeee97345@gmail.com>
2022-12-29 21:53:08 +0300

    //

---
+++ Task29/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int[] array = new int[a];
+for (int i = 0; i < a; i++)
+{
+    array[i] = new Random().Next(a+1);
+    Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
